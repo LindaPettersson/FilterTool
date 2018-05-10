0e5c76d7a3a4856f15d5bcaa325824149f25a821
Linda Pettersson <pettersson.linda08@gmail.com>
2018-05-10 08:55:34 +0200

    added choosefiles

--- FilterApp/WebContent/client/js/app.js
+++ FilterApp/WebContent/client/js/app.js
@@ -2,6 +2,7 @@ var myApp = angular.module('myApp', ['ngRoute']).
 config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
 	$routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'homeController'});
 	$routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'loginController'});
+	$routeProvider.when('/choosefiles', {templateUrl: 'partials/choosefiles.html', controller: 'choosefilesController'});
 	$routeProvider.otherwise({redirectTo: '/home'});
 	
 	//remove hashtag from url
---
+++ FilterApp/WebContent/client/js/controllers/choosefilesController.js
@@ -0,0 +1,3 @@
+myApp.controller('choosefilesController', ['$scope', '$location', function($scope, $location){
+	
+}]);
