6d4f384eca6d6600e0b8a48a8683a22bb2b24710
Linda Pettersson <pettersson.linda08@gmail.com>
2018-05-10 10:39:15 +0200

    fix choosefiles and added choosefilesDirective

--- FilterApp/WebContent/client/js/controllers/choosefilesController.js
+++ FilterApp/WebContent/client/js/controllers/choosefilesController.js
@@ -1,3 +1,13 @@
 myApp.controller('choosefilesController', ['$scope', '$location', function($scope, $location){
+	$scope.files = []; 
 	
+	//Proceed to display files in 'Summary types'
+	$scope.uploadFile = function(){
+		var uploadUrl = "/choosfiles";
+	    fileUpload.uploadFileToUrl(file, uploadUrl);
+		
+	    $location.path('/summarytypes');
+		
+    
+	};
 }]);
