b355d655aad5830e519d687523e43ac7302db36d
Linda Pettersson <pettersson.linda08@gmail.com>
2018-05-21 15:23:49 +0200

    added comment to navController

--- FilterApp/WebContent/client/js/controllers/navController.js
+++ FilterApp/WebContent/client/js/controllers/navController.js
@@ -1,4 +1,6 @@
 myApp.controller('navController', ['$scope', '$location', function($scope, $location){
+	
+	//destination will match location path
 	$scope.isActive = function(destination){
 		return destination === $location.path();
 	}
